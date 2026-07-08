b6e1d03987f1c89df7dee73793a26c9a7f6f429e
unknown <saritakashyap288@gmail.com>
2026-07-08 16:53:06 +0530

    Fix frontend chat endpoint

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ async function sendMessage() {
 
     try {
 
-        const response = await fetch("http://localhost:3000/chat", {
+        const response = await fetch("/chat", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
